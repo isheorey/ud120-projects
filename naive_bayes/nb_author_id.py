7f765694289417fc78e1de4e87ba6d0db88bdbda
Indraneel M Sheorey <indraneelsheorey@gmail.com>
2017-02-25 12:37:22 -0800

    Add timers for training and prediction

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -23,8 +23,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### train classifier
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+t0_train = time()   # start training timer
 clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0_train, 3), "s"
+t0_pred = time()    # start prediction timer
 pred = clf.predict(features_test)
+print "prediction time:", round(time() - t0_pred, 3), "s"
 
 # determine accuracy
 from sklearn.metrics import accuracy_score

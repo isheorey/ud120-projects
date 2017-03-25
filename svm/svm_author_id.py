df95e6e025b4af071110140c08e794091dcd19ab
Indraneel M Sheorey <indraneelsheorey@gmail.com>
2017-03-25 16:56:10 -0700

    Add command line parameters for sampling, kernel, C

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -6,6 +6,11 @@
     Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
+
+    Optional passed arguments:
+    * sampling_percent (default=100)
+    * svm_kernel (default='linear')
+    * svm_C (default=1.0)
 """
 
 import sys
@@ -13,18 +18,31 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+### process arguments
+sampling_rate = 100
+svm_kernel = 'linear'
+svm_C = 1.0
+if sys.argv[1] is not None:
+    sampling_rate = int(sys.argv[1])
+if sys.argv[2] is not None:
+    svm_kernel = sys.argv[2]
+if sys.argv[3] is not None:
+    svm_C = float(sys.argv[3])
+
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 ### sample dataset
-features_train = features_train[:len(features_train) / 100]
-labels_train = labels_train[:len(labels_train) / 100]
+features_train_end = len(features_train) * sampling_rate / 100
+labels_train_end = len(labels_train) * sampling_rate / 100
+features_train = features_train[:features_train_end]
+labels_train = labels_train[:labels_train_end]
 
 ### train classifier
 from sklearn.svm import SVC
-clf = SVC(kernel='rbf')
+clf = SVC(svm_C, svm_kernel)
 t0_train = time()   # start training timer
 clf.fit(features_train, labels_train)
 print "training time:", round(time() - t0_train, 3), "s"

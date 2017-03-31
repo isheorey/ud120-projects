99f2131b2d2effd818cb79b55e291679c82b14d6
Indraneel M Sheorey <indraneelsheorey@gmail.com>
2017-03-30 23:40:03 -0700

    Add AdaBoost training & prediction on terrain data

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 
+"""
+    Implementation of AdaBoost algorithm on terrain dataset
+
+    Optional passed arguments:
+    * n_estimators (default=50)
+"""
+import sys
 import matplotlib.pyplot as plt
+from time import time
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
 
-features_train, labels_train, features_test, labels_test = makeTerrainData()
+### process arguments
+ada_n_estimators = 50
+if len(sys.argv) > 1:
+    n_estimators = int(sys.argv[1])
 
+print "# of estimators:", ada_n_estimators
+
+### generate terrain data
+features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 ### the training data (features_train, labels_train) have both "fast" and "slow"
 ### points mixed together--separate them so we can give them different colors
@@ -15,8 +30,7 @@ bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
-
-#### initial visualization
+### initial visualization
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
 plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
@@ -25,19 +39,23 @@ plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
-################################################################################
 
+### train classifier
+from sklearn.ensemble import AdaBoostClassifier
+clf = AdaBoostClassifier(n_estimators=ada_n_estimators)
+t0_train = time()   # start training timer
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0_train, 3), "s"
+t0_pred = time()    # start prediction timer
+pred = clf.predict(features_test)
+print "prediction time:", round(time() - t0_pred, 3), "s"
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+# determine accuracy
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print "accuracy:", accuracy
 
-
-
-
-
-
-
-
+### plot pretty picture
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
